e171e7e79e964cff9a1ebf833972c17c7993153e
AMacro <andrew@andrewcraigmackenzie.com>
2025-06-22 10:49:51 +1000

    Refactor MultiplayerAPITest loading workflow

--- MultiplayerAPI Tests/MultiplayerAPITest.cs
+++ MultiplayerAPI Tests/MultiplayerAPITest.cs
@@ -21,19 +21,13 @@ public static class MultiplayerAPITest
         //Settings = Settings.Load(modEntry);
         //ModEntry.OnGUI = Settings.Draw;
         //ModEntry.OnSaveGUI = Settings.Save;
-        //ModEntry.OnLateUpdate = LateUpdate;
+        ModEntry.OnLateUpdate = LateUpdate;
 
         Harmony harmony = null;
 
         try
         {
             Log($"Multiplayer Mod is loaded: {MultiplayerAPI.IsMultiplayerLoaded}");
-
-            if (MultiplayerAPI.IsMultiplayerLoaded)
-            {
-                MultiplayerAPI.ServerStarted += OnServerStarted;
-                MultiplayerAPI.ClientStarted += OnClientStarted;
-            }
 
             Log("Patching...");
             harmony = new Harmony(ModEntry.Info.Id);
@@ -51,14 +45,42 @@ public static class MultiplayerAPITest
         return true;
     }
 
+    
+    private static void LateUpdate(UnityModManager.ModEntry modEntry, float dt)
+    {
+        //Mod loading order can't be guaranteed, so we should wait for all mods to load prior to checking for multiplayer.
+        //Alternatively, set 'Multiplayer' in the 'LoadAfter' parameter in your 'info.json'
+        Log($"MultiplayerAPITest.LateUpdate() Multiplayer Mod is loaded: {MultiplayerAPI.IsMultiplayerLoaded}");
+        if (MultiplayerAPI.IsMultiplayerLoaded)
+        {
+            //Register that this mod needs to be installed on both server and client
+            MultiplayerAPI.Instance.SetModCompatibility(ModEntry.Info.Id, MultiplayerCompatibility.All);
+
+            // Register for server start and client start events
+            // Note: for a non dedicated server both server and client events will be fired
+            MultiplayerAPI.ServerStarted += OnServerStarted;
+            MultiplayerAPI.ClientStarted += OnClientStarted;
+        }
+
+        modEntry.OnLateUpdate -= LateUpdate;
+    }
+
     private static void OnServerStarted(IServer server)
     {
+        // How you handle the server starting is up to you
+        // In this test/example mod we are injecting a server manager into the scene, but you could
+        // also just integrate it into your mod's existing workflow.
+        // Keep in mind on a non-dedicated server, both the client and server will run concurrently
         GameObject go = new GameObject("MPAPI ServerTest", [typeof(ServerTest)]);
         GameObject.DontDestroyOnLoad(go);
     }
 
     private static void OnClientStarted(IClient client)
     {
+        // How you handle the client starting is up to you
+        // In this test/example mod we are injecting a client manager into the scene, but you could
+        // also just integrate it into your mod's existing workflow.
+        // Keep in mind on a non-dedicated host, both the client and server will run concurrently
         GameObject go = new GameObject("MPAPI ClientTest", [typeof(ClientTest)]);
         GameObject.DontDestroyOnLoad(go);
     }
